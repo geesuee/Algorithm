d582535d666514d449bca476ecf925c464a3e14a
geesuee <baejisu97@gmail.com>
2021-10-04 08:52:16 +0900

    2021/10/03 알고리즘 공부

--- ET_algorithmStudy/src/week12/algorithm/Q2_3190.java
+++ ET_algorithmStudy/src/week12/algorithm/Q2_3190.java
@@ -1,59 +1,144 @@
 // [백준] 뱀
 // https://www.acmicpc.net/problem/3190
 
-// 아직 풀지 못함..!
-
 package week12.algorithm;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Q2_3190 {
-    static int N,K,L;
-    static int[][] location;
-    static String[][] direction;
+    static int n; // 보드의 크기 (2~100)
+    static int k; // 사과의 갯수 (0~100)
+    static int l; // 뱀의 방향 변환 횟수 (1~100)
+    static int time; // 게임 시간
+    static int[][] board;
+
+    static List<int[]> snake; //뱀의 몸통 위치 (x,y)
+
+    // 처음 시작은 오른쪽 방향을 보고 있음
+    // 0:오른쪽   1:아래쪽   2:왼쪽   3:위
+    // static int[] dx = {0, 1, 0, -1};
+    // static int[] dy = {1, 0, -1, 0};
+    // (0,1) -> (1,0) -> (0,-1) -> (-1,0) 씩 더하면서 한 바퀴 이동
+    // D(오른쪽)가 다오면 index++
+    // L(왼쪽)이 나오면 index--
+    static int index = 0;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    static HashMap<Integer, String> dir; // 뱀의 방향 정보
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine()); //보드의 크기
-        K = Integer.parseInt(br.readLine()); //사과의 개수
+        n = Integer.parseInt(br.readLine()); // 보드의 크기
+        k = Integer.parseInt(br.readLine()); // 사과의 개수
 
-        location = new int[N][N]; //사과 위치
-        for(int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            location[a][b] = 1;
+        // 1,1이 맨 위 맨 좌측
+        board = new int[n+1][n+1];
+
+        // 사과 위치 입력
+        String str;
+        int row; // 행
+        int col; // 열
+        for(int i=0; i<k; i++) {
+            str = br.readLine();
+
+            row = Integer.parseInt(str.split(" ")[0]);
+            col = Integer.parseInt(str.split(" ")[1]);
+
+            board[row][col] = 1;
         }
 
-        L = Integer.parseInt(br.readLine()); //뱀의 방향 변환 횟수
-        direction = new String[L][1];
-        for(int i = 0; i < L; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            direction[i][0] = st.nextToken();
-            direction[i][1] = st.nextToken();
+        // 뱀 방향 정보 입력
+        dir = new HashMap<>();
+        l = Integer.parseInt(br.readLine());
+        for(int i=0; i<l; i++) {
+            str = br.readLine();
+            int timeInfo = Integer.parseInt(str.split(" ")[0]);
+            String directionInfo = str.split(" ")[1];
+
+            dir.put(timeInfo, directionInfo);
         }
 
-        Snake snake = new Snake(L);
+        // 뱀 시작 지점 (1,1) (x,y)
+        snake = new LinkedList<>();
+        snake.add(new int[]{1,1});
 
-        System.out.println(dummy());
+        time = 0;
+        int nx, ny; // 다음 움직임
+        int cx, cy; // 현재 움직임(1,1)
+        cx = 1;
+        cy = 1;
+        // 뱀 움직임 시작
+        while(true) {
+            time++;
+
+            // 다음 움직임(머리 데이터)
+            // 처음 시작은 오른쪽 방향을 보고 있음(index = 0)
+            // 0:오른쪽   1:아래쪽   2:왼쪽   3:위
+            // static int[] dx = {0, 1, 0, -1};
+            // static int[] dy = {1, 0, -1, 0};
+            nx = cx + dx[index]; // 첫번째 반복에서는 1
+            ny = cy + dy[index]; // 첫번째 반복에서는 2
+            // (1,1) -> (1,2) 이동
+
+            // 끝나는지 확인
+            if(isFinish(nx,ny)) break;
+
+            // 사과 있는지 확인
+            // 사과 있으면 사과 없어지고 머리 늘려서 꼬리 그대로
+            if(board[nx][ny] == 1) {
+                board[nx][ny] = 0;
+                snake.add(new int[] {nx,ny}); // 머리 정보 추가
+            }
+            // 사과 없으면 머리 늘려서 꼬리가 위치한 칸 비워줌
+            else {
+                snake.add(new int[] {nx,ny}); // 머리 정보 추가
+                snake.remove(0); // 꼬리 index는 0
+            }
+
+            cx = nx;
+            cy = ny;
+
+            // 해당 시간 끝났을 때 다음 방향 정해주기
+            if(dir.containsKey(time)) {
+                // D(오른쪽)가 나오면 index++
+                if(dir.get(time).equals("D")) {
+                    index++;
+                    if(index == 4)
+                        index = 0;
+                }
+                // L(왼쪽)이 나오면 index--
+                if(dir.get(time).equals("L")) {
+                    index--;
+                    if(index == -1)
+                        index = 3;
+                }
+
+            }
+        }
+
+        System.out.println(time);
     }
 
-    public static class Snake {
-        int length;
-        int move;
+    // 게임이 끝나는지 확인
+    static boolean isFinish(int nx, int ny){
 
-        Snake(int L) {
-            this.length = 1;
-            this.move = L;
+        // 벽에 부딪히거나
+        if(nx<1 || ny<1 || nx>=n+1 || ny>=n+1)
+            return true;
+
+        // 자기 몸통에 닿거나
+        for(int i=0; i<snake.size(); i++) {
+            if(nx == snake.get(i)[0] && ny == snake.get(i)[1])
+                return true;
         }
-    }
 
-    static int dummy() {
-        return 0;
+        return false;
     }
 }
--- PlayData_algorithmStudy/src/week9/greedy/Q3_42885.java
+++ PlayData_algorithmStudy/src/week9/greedy/Q3_42885.java
@@ -1,4 +1,21 @@
+// [프로그래머스] 구명보트
+// https://programmers.co.kr/learn/courses/30/lessons/42885
+// 미완
+
 package week9.greedy;
 
 public class Q3_42885 {
+
+    public static void main(String[] args) {
+        int[] people = {100,500,500,900,950}; //500,500/100,900/950
+        int limit = 1000;
+
+        System.out.println(solution(people,limit));
+    }
+
+    public static int solution(int[] people, int limit) {
+
+        int answer = 0;
+        return answer;
+    }
 }
