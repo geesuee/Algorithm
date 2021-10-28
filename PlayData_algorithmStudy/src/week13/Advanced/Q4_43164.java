9319130ab89b81f44ce9888de8ae931dab1c096c
geesuee <baejisu97@gmail.com>
2021-10-28 15:55:55 +0900

    2021/10/28 알고리즘 공부

--- PlayData_algorithmStudy/src/week13/Advanced/Q4_43164.java
+++ PlayData_algorithmStudy/src/week13/Advanced/Q4_43164.java
@@ -3,10 +3,12 @@
 
 package week13.Advanced;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Q4_43164 {
+    static boolean[] visited;                    //방문한지 안한지를 체크하는 visited배열
+    static ArrayList<String> answers;
 
     public static void main(String[] args) {
         String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
@@ -18,26 +20,25 @@ public class Q4_43164 {
     }
 
     public static String[] solution(String[][] tickets) {
-        String[] answer = new String[tickets.length+1];
-        String start = "ICN";
-        PriorityQueue<String> canGo = new PriorityQueue<>();
-        answer[0] = start;
-
-        // 이동 횟수에 대한 큰 반복
-        for(int i = 0; i < tickets.length; i++) {
-            // 어디로 이동할 수 있는지 작은 반복
-            for(String[] t : tickets) {
-                if(t[0].equals(start)) {
-                    canGo.add(t[1]);
-                }
-            }
-
-            start = canGo.poll();  // 알파벳순 가장 앞 공항 뽑아 이동
-            answer[i+1] = start;
-            canGo.clear();  // 다음 이동 공항 탐색 전 canGo 초기화
-            Arrays.stream(tickets).anyMatch()//미완
-        }
-
+        visited = new boolean[tickets.length];
+        answers = new ArrayList<String>();
+        int count = 0;                                    //몇개의 공항을 들렸는지 알려주는 카운트
+        dfs(count, "ICN", "ICN",tickets);
+        Collections.sort(answers);                        //답들 중 가장 알파벳순이 빠른 배열들로 정렬
+        String[] answer = answers.get(0).split(" ");    //가장 빠른 배열을 뽑아서 String형 배열로 만듬
         return answer;
+    }
+    public static void dfs(int count, String present, String answer, String[][]ticktes) {
+        if(count == ticktes.length) {            //모든 공항을 들렸다면
+            answers.add(answer);                //answers에 추가
+            return;
+        }
+        for(int i = 0; i < ticktes.length; i++) {
+            if(!visited[i] && ticktes[i][0].equals(present)) {        //present와 같고 들리지 않은 공항을 찾고
+                visited[i] = true;                                    //해당 공항을 들린걸로 만듬.
+                dfs(count+1, ticktes[i][1],answer+" "+ticktes[i][1] , ticktes);    //카운트 +1 도착 공항을 present로 넣어주고 answer에 도착공항을 추가해준다.
+                visited[i] = false;
+            }
+        }
     }
 }
