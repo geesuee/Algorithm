ba1c5d197bb9b288622b89f6be9db30ea57a7f11
geesuee <baejisu97@gmail.com>
2021-11-17 16:25:46 +0900

    2021/11/17 알고리즘 공부

--- PlayData_algorithmStudy/src/week16/Q1_8983.java
+++ PlayData_algorithmStudy/src/week16/Q1_8983.java
@@ -1,42 +1,80 @@
 // [백준] 사냥꾼
 // https://www.acmicpc.net/problem/8983
-// 미완
+// 이분 탐색
 
 package week16;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q1_8983 {
+    static int M, N, L;
+    static int[] gunspot;
+    static int[][] animalspot;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int M = Integer.parseInt(st.nextToken());  // 사대의 수
-        int N = Integer.parseInt(st.nextToken());  // 동물의 수
-        int L = Integer.parseInt(st.nextToken());  // 사정 거리
+        M = Integer.parseInt(st.nextToken());  // 사대의 수
+        N = Integer.parseInt(st.nextToken());  // 동물의 수
+        L = Integer.parseInt(st.nextToken());  // 사정 거리
 
         // 사대 위치
-        long[][] gunspot = new long[M][2];
+        gunspot = new int[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            gunspot[i][0] = Long.parseLong(st.nextToken());
-            gunspot[i][1] = Long.parseLong(st.nextToken());
+            gunspot[i] = Integer.parseInt(st.nextToken());
         }
 
         // 동물 위치
-        long[][] animalspot = new long[N][2];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            animalspot[i][0] = Long.parseLong(st.nextToken());
-            animalspot[i][1] = Long.parseLong(st.nextToken());
+        animalspot = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            animalspot[i][0] = Integer.parseInt(st.nextToken());
+            animalspot[i][1] = Integer.parseInt(st.nextToken());
         }
 
-
+        System.out.println(solution());
     }
 
+    static int solution() {
+        int cnt = 0;
+        Arrays.sort(gunspot);
 
+        for(int i = 0; i < N; i++) {
+            cnt += search(i);
+        }
+
+        return cnt;
+    }
+
+    static int search(int idx) {
+        int start = 0;
+        int end = M;
+        int mid = 0;
+
+        while(start <= end) {
+            mid = (start + end) / 2;
+
+            if(mid >= M) {
+                return 0;
+            }
+
+            // x좌표값 차의 절대값에 y좌표값을 더해 사대와 동물 간 거리 계산
+            int distance = Math.abs(gunspot[mid] - animalspot[idx][0]) + animalspot[idx][1];
+
+            if (L < distance && animalspot[idx][0] < gunspot[mid]) {
+                end = mid - 1;
+            }else if (L < distance && animalspot[idx][0] >= gunspot[mid]) {
+                start = mid + 1;
+            }else if (L >= distance) {
+                return 1;
+            }
+        }
+        return 0;
+    }
 }
